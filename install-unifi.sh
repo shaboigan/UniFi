5b41a391fef0e74a8e4811d7c17769fa652a03ed
shaboigan <geof.grouch.88@gmail.com>
2019-05-17 13:21:30 +1000

    Updated scripts to add teamviewer

--- install-unifi.sh
+++ install-unifi.sh
@@ -16,6 +16,12 @@ passwd
 echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
+echo -e "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
+wget wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer.deb
+
+echo -e "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
+sudo dpkg -i teamviewer.deb; sudo apt-get install -f -y
+
 echo -e "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
