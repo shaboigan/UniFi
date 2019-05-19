33a077c1d68798ae6f2fc1b1c28e936af522c4f7
shaboigan <geof.grouch.88@gmail.com>
2019-05-20 09:51:27 +1000

    Tweaks

--- install-unifi-pihole.sh
+++ install-unifi-pihole.sh
@@ -3,41 +3,41 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo "${Colour}By using this script, you'll remove the default pi account, update the system, install Teamviewer, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
+echo -e "${Colour}By using this script, you'll remove the default pi account, update the system, install Teamviewer, install the stable UniFi controller of your choice and install Pi-hole.\nUse CTRL+C to cancel the script\n\n${less}"
 read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.21: " version
 
 if [[ -z "$version" ]]; then
 	version='5.10.21'
 fi
 
-echo "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
+echo -e "${Colour}\nRemoving the default pi account and the associated home directory\n${less}"
 sudo deluser pi
 
-echo "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
+echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
-echo "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
 wget wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer.deb
 
-echo "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
 sudo dpkg -i teamviewer.deb; sudo apt-get install -f -y
 
-echo "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
+echo -e "${Colour}\n\nThe UniFi controller with version $version is downloading now.\n\n${less}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
-echo "${Colour}\n\nBefore installing the UniFi Controller, it will first install OpenJDK 8.\n\n${less}"
+echo -e "${Colour}\n\nBefore installing the UniFi Controller, it will first install OpenJDK 8.\n\n${less}"
 sudo apt-get install openjdk-8-jre-headless -y
 
-echo "${Colour}\n\nIn order to fix an issue which can cause a slow start for the UniFi controller, haveged is installed.\n\n${less}"
+echo -e "${Colour}\n\nIn order to fix an issue which can cause a slow start for the UniFi controller, haveged is installed.\n\n${less}"
 sudo apt-get install haveged -y
 
-echo "${Colour}\n\nThe UniFi controller will be installed now.\n\n${less}"
+echo -e "${Colour}\n\nThe UniFi controller will be installed now.\n\n${less}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb; sudo apt-get install -f -y
 
 if [[ -z "$1" ]] ; then
-echo "${Colour}\n\nPi-hole will be installed now.\nThe initial configuration is interactive.\n\n${less}"
+echo -e "${Colour}\n\nPi-hole will be installed now.\nThe initial configuration is interactive.\n\n${less}"
 curl -sSL https://install.pi-hole.net | bash
 
-echo "${Colour}\n\nOne more step is changing the password for the web interface of the Pi-hole.\n\n${less}"
+echo -e "${Colour}\n\nOne more step is changing the password for the web interface of the Pi-hole.\n\n${less}"
 pihole -a -p
 fi
--- update-unifi.sh
+++ update-unifi.sh
@@ -3,23 +3,23 @@
 Colour='\033[1;31m'
 less='\033[0m'
 
-echo "${Colour}By using this script you will UPGRADE your system and the UniFi Controller.\n${less}"
+echo -e "${Colour}By using this script you will UPGRADE your system and the UniFi Controller.\n${less}"
 read -p "Please enter the STABLE version (e.g: 5.9.29) or press enter for version 5.10.21: " version
 
 if [[ -z "$version" ]]; then
 	version='5.10.21'
 fi
-echo "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
+echo -e "${Colour}\n\nThe system will now upgrade all the software and firmware, as well as clean up old/unused packages.\n\n${less}"
 sudo apt-get update && sudo apt-get upgrade -y && sudo apt-get autoremove && sudo apt-get autoclean
 
-echo "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer is downloading now.\n\n${less}"
 wget wget https://download.teamviewer.com/download/linux/teamviewer-host_armhf.deb -O teamviewer.deb
 
-echo "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
+echo -e "${Colour}\n\nTeamviewer will be installed now.\n\n${less}"
 sudo dpkg -i teamviewer.deb; sudo apt-get install -f -y
 
-echo "${Colour}\n\nThe UniFi controller (version $version) will now be downloaded.\n\n${less}"
+echo -e "${Colour}\n\nThe UniFi controller (version $version) will now be downloaded.\n\n${less}"
 wget http://dl.ubnt.com/unifi/$version/unifi_sysvinit_all.deb -O unifi_$version\_sysvinit_all.deb
 
-echo "${Colour}\n\nThe UniFi controller will now be upgraded.\n\n${less}"
+echo -e "${Colour}\n\nThe UniFi controller will now be upgraded.\n\n${less}"
 sudo dpkg -i unifi_$version\_sysvinit_all.deb
