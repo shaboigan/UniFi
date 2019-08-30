776d8882c3f140646f11e0c63852942b569509fd
shaboigan <geof.grouch.88@gmail.com>
2019-08-30 16:57:18 +1000

    tweaks

--- UniFi-Windows-Service/unifi-upgrade.ps1
+++ UniFi-Windows-Service/unifi-upgrade.ps1
@@ -1,4 +1,35 @@
 Set-ExecutionPolicy AllSigned
+
+$multiplelines = @"
+
+#########################################################################
+#                                                                       #
+#                  KM                       ,ok0KNWW                    #
+#                        KM               :NMMMMMMMM                    #
+#                       KM  ..             WMMMMMMMMM                   #
+#                   KM      KM             WMMMMMMMMM                   #
+#                   KM    KM               WMMMMMMMMM                   #
+#                   KM  KM  ..             WMMMMMMMMM                   #
+#                   KM  ..  KM             WMMMMMMMMM                   #
+#                   KM  KM  KM             WMMMMMMMMM                   #
+#                   KMNXWM  KM             WMMMMMMMMK                   #
+#                   KMMMMMKONM             WMMMMMMMW                    #
+#                   KMMMMMMMMM             WMMMMMMM x                   #
+#                   lMMMMMMMMM             WMMMMMN xK                   #
+#                    MMMMMMMMMl           ,WMMMP dXM:                   #
+#                    lMMMMMMMMx .        ,,,aaadXMMd                    #
+#                     lNMMMMMMW: XOxolcclodOKMMMMWc                     #
+#                       lXMMMMMNc lMMMMMMMMMMMMNo.                      #
+#                         llONMMM0c lMMMMMMNOo'                         #
+#                              'lMN;. lMWl'                             #
+#                                                                       #
+#        Unifi controller as a Windows Service Upgrade Installer        #
+#########################################################################
+
+"@
+
+Write-Host $multiplelines
+
 echo "Unifi Upgrade Script"
 echo "Uninstalling previous Unifi Service"
 Get-ChildItem "C:\Program Files\Java" -recurse | where {$_.Name -eq "java.exe"} | % {
--- UniFi-Windows-Service/unifi_install.ps1
+++ UniFi-Windows-Service/unifi_install.ps1
@@ -1,5 +1,35 @@
 Set-ExecutionPolicy AllSigned
-echo "Unifi Install script"
+
+$multiplelines = @"
+
+#########################################################################
+#                                                                       #
+#                  KM                       ,ok0KNWW                    #
+#                        KM               :NMMMMMMMM                    #
+#                       KM  ..             WMMMMMMMMM                   #
+#                   KM      KM             WMMMMMMMMM                   #
+#                   KM    KM               WMMMMMMMMM                   #
+#                   KM  KM  ..             WMMMMMMMMM                   #
+#                   KM  ..  KM             WMMMMMMMMM                   #
+#                   KM  KM  KM             WMMMMMMMMM                   #
+#                   KMNXWM  KM             WMMMMMMMMK                   #
+#                   KMMMMMKONM             WMMMMMMMW                    #
+#                   KMMMMMMMMM             WMMMMMMM x                   #
+#                   lMMMMMMMMM             WMMMMMN xK                   #
+#                    MMMMMMMMMl           ,WMMMP dXM:                   #
+#                    lMMMMMMMMx .        ,,,aaadXMMd                    #
+#                     lNMMMMMMW: XOxolcclodOKMMMMWc                     #
+#                       lXMMMMMNc lMMMMMMMMMMMMNo.                      #
+#                         llONMMM0c lMMMMMMNOo'                         #
+#                              'lMN;. lMWl'                             #
+#                                                                       #
+#          Unifi controller as a Windows Service Installer              #
+#########################################################################
+
+"@
+
+Write-Host $multiplelines
+
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y ubiquiti-unifi-controller --version 5.10.25
 echo "Removing default shortcuts"
