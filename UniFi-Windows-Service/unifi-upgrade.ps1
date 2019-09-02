a0eb4c5807ce6547d67b68d6bd2ff9df5a89b324
shaboigan <geof.grouch.88@gmail.com>
2019-09-02 11:41:28 +1000

    tweaks

--- UniFi-Windows-Service/unifi-upgrade.ps1
+++ UniFi-Windows-Service/unifi-upgrade.ps1
@@ -39,9 +39,7 @@ Set-Location "$env:USERPROFILE\Ubiquiti UniFi\"
 Get-Service "UniFi*" | Stop-Service
 & $x64Java -jar lib\ace.jar uninstallsvc
 choco upgrade -y ubiquiti-unifi-controller --version 5.10.27
-echo "Removing default shortcuts"
-Remove-Item –path '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Ubiquiti UniFi' –recurse
-Remove-Item –path '$env:USERPROFILE\Desktop\Unifi.lnk' –recurse
+
 echo "Installing Unifi Service"
 Get-ChildItem "C:\Program Files\Java" -recurse | where {$_.Name -eq "java.exe"} | % {
 $x64Java = $_.FullName
@@ -49,4 +47,8 @@ $x64Java = $_.FullName
 Set-Location "$env:USERPROFILE\Ubiquiti UniFi\"
 & $x64Java -jar lib\ace.jar installsvc
 Get-Service "UniFi*" | Start-Service
+
+echo "Removing default shortcuts"
+Remove-Item –path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Ubiquiti UniFi" –recurse
+Remove-Item –path "$env:USERPROFILE\Desktop\Unifi.lnk" –recurse
 exit
--- UniFi-Windows-Service/unifi_install.ps1
+++ UniFi-Windows-Service/unifi_install.ps1
@@ -32,9 +32,7 @@ Write-Host $multiplelines
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 choco install -y ubiquiti-unifi-controller --version 5.10.25
-echo "Removing default shortcuts"
-Remove-Item –path '$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Ubiquiti UniFi' –recurse
-Remove-Item –path '$env:USERPROFILE\Desktop\Unifi.lnk' –recurse
+
 echo "Installing Unifi Service"
 Get-ChildItem "C:\Program Files\Java" -recurse | where {$_.Name -eq "java.exe"} | % {
 $x64Java = $_.FullName
@@ -42,4 +40,8 @@ $x64Java = $_.FullName
 Set-Location "$env:USERPROFILE\Ubiquiti UniFi\"
 & $x64Java -jar lib\ace.jar installsvc
 Get-Service "UniFi*" | Start-Service
+
+echo "Removing default shortcuts"
+Remove-Item –path "$env:APPDATA\Microsoft\Windows\Start Menu\Programs\Ubiquiti UniFi" –recurse
+Remove-Item –path "$env:USERPROFILE\Desktop\Unifi.lnk" –recurse
 exit
