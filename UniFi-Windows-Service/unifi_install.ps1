be1e2340e06e96aaa1f34f9cfdcc42ce7c676f31
shaboigan <geof.grouch.88@gmail.com>
2019-09-04 11:06:24 +1000

    Update unifi_install.ps1

--- UniFi-Windows-Service/unifi_install.ps1
+++ UniFi-Windows-Service/unifi_install.ps1
@@ -31,7 +31,7 @@ $multiplelines = @"
 Write-Host $multiplelines
 
 Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
-choco install -y ubiquiti-unifi-controller --version 5.10.25
+choco install -y ubiquiti-unifi-controller --version 5.10.25 --force
 
 echo "Installing Unifi Service"
 Get-ChildItem "C:\Program Files\Java" -recurse | where {$_.Name -eq "java.exe"} | % {
